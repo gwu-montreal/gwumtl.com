7841ab7bfa23a99cc344f8600f590621cad49530
gwuzyx <gwuzyx@users.noreply.github.com>
2020-12-03 00:16:16 -0500

    make grid.module.css pure by moving vars to global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,46 +1,49 @@
 import React from "react";
 import Head from "next/head";
-import App from "next/app";
+import { useRouter } from "next/router";
 
 import Header from "~/components/Header";
 
 import "~/styles/index.css";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+import type { AppProps } from "next/app";
 
-    return (
-      <>
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-          {/* <script async src="/fathom.js" /> */}
-        </Head>
-        {/* <Header /> */}
-        <Component {...pageProps} />
-      </>
-    );
-  }
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { query } = useRouter();
+
+  // if there's no lang, we're on the admin page; render it directly
+  return query.lang ? (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        {/* <script async src="/fathom.js" /> */}
+      </Head>
+      {/* <Header /> */}
+      <Component {...pageProps} />
+    </>
+  ) : (
+    <Component {...pageProps} />
+  );
+};
 
 export default MyApp;
