1cf5db3642f89211cdcd9f25beaa4b953b7ea693
gwuzyx <gwuzyx@users.noreply.github.com>
2020-12-05 22:24:17 -0500

    fixup grid

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -5,6 +5,13 @@ const { box } = css`
   .box {
     background-color: #f4f4f4;
     box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-bottom: 2rem;
+
+    padding: 2rem 1rem;
+
+    @media (--sm) {
+      padding: 2rem 0;
+    }
   }
 `;
 
