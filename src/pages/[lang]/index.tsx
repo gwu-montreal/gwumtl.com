7d33718828f712f84e5eca6de9d2c158b6343304
gwuzyx <gwuzyx@users.noreply.github.com>
2021-05-05 19:31:41 -0400

    prettier

--- src/pages/[lang]/index.tsx
+++ src/pages/[lang]/index.tsx
@@ -47,33 +47,54 @@ const Index = ({ sections, description }: PageProps) => {
       <div className="max-w-container mx-auto px-8 mt-16 lg:px-16 lg:mt-24">
         {sections.map(({ type, content, image, imagePlacement }, i) => {
           return (
-            <div key={i} className={cx(
-              "mb-16 lg:mb-24",
-              {"shadow-lg bg-gray-50 p-8 -mx-8 lg:px-16 lg:py-12 lg:-mx-16": type === "box"}
-            )}>
-              <div className={cx("flex flex-wrap", type === "box" ? "md:flex-nowrap": "sm:block lg:flex lg:flex-nowrap")}>
+            <div
+              key={i}
+              className={cx("mb-16 lg:mb-24", {
+                "shadow-lg bg-gray-50 p-8 -mx-8 lg:px-16 lg:py-12 lg:-mx-16":
+                  type === "box",
+              })}
+            >
+              <div
+                className={cx(
+                  "flex flex-wrap",
+                  type === "box"
+                    ? "md:flex-nowrap"
+                    : "sm:block lg:flex lg:flex-nowrap"
+                )}
+              >
                 <div
                   className={cx(
                     "sm:flex-shrink-0 mx-auto order-3",
-                    {"sm:order-1": imagePlacement === "left"},
+                    { "sm:order-1": imagePlacement === "left" },
                     type === "box"
                     ? "mt-6 md:mt-0 md:pl-6 md:w-2/5"
                     : [
                       "mt-6 sm:mt-0 sm:mb-4 sm:w-2/5 md:w-80 lg:float-none lg:mb-0",
-                      {"sm:float-left sm:ml-0 sm:mr-8": imagePlacement === "left"},
-                      {"sm:float-right sm:ml-8 sm:mr-0": imagePlacement === "right"}
+                          {
+                            "sm:float-left sm:ml-0 sm:mr-8":
+                              imagePlacement === "left",
+                          },
+                          {
+                            "sm:float-right sm:ml-8 sm:mr-0":
+                              imagePlacement === "right",
+                          },
                     ]
                   )}
                 >
-                  {image && <Pic className={cx(
+                  {image && (
+                    <Pic
+                      className={cx(
                     "mx-auto",
                     type === "box" ? "max-h-64" : "max-h-56"
-                  )} imgName={image} />}
+                      )}
+                      imgName={image}
+                    />
+                  )}
                 </div>
                 <div
                   className={cx(
                     "prose break-words max-w-full mx-auto md:mx-0 order-2",
-                    {"text-xl": type === "box"}
+                    { "text-xl": type === "box" }
                   )}
                   dangerouslySetInnerHTML={{ __html: content }}
                 />
