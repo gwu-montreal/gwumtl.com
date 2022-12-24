3aca96af5b81e4316c5c3e67e463dcbe6a137e35
gwuzyx <gwuzyx@users.noreply.github.com>
2022-12-24 02:22:46 -0500

    add seo/card for bare domain

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,28 @@
 import Script from "next/script";
 
-export default function Custom404() {
+import SEO from "~/components/SEO";
+
+import type { GetStaticProps } from "next";
+
+interface PageProps {
+  description: string;
+}
+
+export default function Custom404({ description }: PageProps) {
   return (
-    <Script strategy="beforeInteractive">
-      {`if(navigator.language.split('-')[0] === 'fr') location.href = 'fr'; else location.href = 'en'`}
-    </Script>
+    <>
+      <Script strategy="beforeInteractive">
+        {`if(navigator.language.split('-')[0] === 'fr') location.href = 'fr'; else location.href = 'en'`}
+      </Script>
+      <SEO title="GWU Montréal" omitSiteNameInTitle description={description} />
+    </>
   );
 }
+
+// adapted from `[lang]/index.tsx`, could be simplified!
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const { loadYaml } = await import("~/lib/load-mdx");
+  const path = `content/pages/home/home.en.yml`;
+  const contents = await loadYaml(path, ["description"]);
+  return { props: { description: contents["description"] } };
+};
